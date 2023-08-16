9a8b38812183f9b9e9d5fc4b40287c841d478af8
Rodrigo Costa <ccostarodrigoo@gmail.com>
2023-08-16 12:23:19 -0300

    Update Main.java

--- src/com/rodjao/me/Main.java
+++ src/com/rodjao/me/Main.java
@@ -4,9 +4,7 @@ import com.rodjao.me.commands.HelloCom;
 
 import com.rodjao.me.commands.Sethome;
 import com.rodjao.me.commands.Teleporthome;
-import com.rodjao.me.events.BedEvent;
-import com.rodjao.me.events.ExitEvent;
-import com.rodjao.me.events.FirstEvent;
+
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -16,10 +14,7 @@ public class Main extends JavaPlugin {
     public void onEnable(){
         instance = this;
         System.out.println("Mariana eh linda");
-        Bukkit.getPluginManager().registerEvents(new FirstEvent(), this);
-        Bukkit.getPluginManager().registerEvents(new BedEvent(), this);
-        Bukkit.getPluginManager().registerEvents(new ExitEvent(), this);
-        getCommand("hello").setExecutor(new HelloCom());
+        
         getCommand("sethome").setExecutor(new Sethome());
         getCommand("telhome").setExecutor(new Teleporthome());
 
